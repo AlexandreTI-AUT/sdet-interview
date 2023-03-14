0c506341e5aacf754be9639fbd6e1da138c5f252
Alexandre Silva <alexix.dasilva@gmail.com>
2023-03-14 15:01:10 -0300

    Living Code

--- cypress/e2e/ProductTest.cy.js
+++ cypress/e2e/ProductTest.cy.js
@@ -3,29 +3,35 @@ import { HomePage } from "../support/pages/HomePage/HomePage";
 import { SearchPage } from "../support/pages/SearchPage/SearchPage";
 
 describe("Search for test products", () => {
+  beforeEach(() => {
+    cy.visit("https://loja.inventa.shop/");
+  });
+
   it("Will search for batom and we expect to get more than one product and validate if for the first product a price is visible and after that I want to do the login", () => {
-    cy.vsit("https://loja.inventa.shop/");
-
     cy.get('[data-testid="search-bar"]').type("batom");
+    cy.get('[data-testid="search-icon"]').click();
 
-    cy.wait(6000);
+    // cy.wait(6000);
     cy.get('[name="product-card"]').then((elements) => {
-      expect(elements).to.be.greaterThan(1);
+      expect(elements.length).to.be.greaterThan(1);
     });
-    cy.get('[name="product-card"]').eq(0).should("exist");
+    cy.get('[name="product-card"]').eq(0).should("be.visible");
 
-    cy.click(cy.contains("Entre para comprar"));
+    cy.get(".next-18zsgw9 > :nth-child(1) > .chakra-button")
+      .eq(0)
+      .click()
+      .should("contain", "Entre para comprar");
 
     cy.url().should("contain", "login");
   });
 
   it("Search for batom, click in the first produt and check if description is greater than 0", () => {
     cy.visit("https://loja.inventa.shop/");
-    let page = new HomePage();
+    const page = new HomePage();
     page.getSearchBarInput().type("batom");
 
     cy.url().should("contain", "q=batom");
-    page = new SearchPage();
+    const page2 = new SearchPage();
     cy.get('[name="product-card"]').first().children().first().click();
     cy.get('[data-testid="description"]')
       .children()
---
+++ cypress/support/pages/SearchPage/SearchPageSeletors.js
@@ -0,0 +1,3 @@
+import { SEARCH_BAR_INPUT_CLASS } from "./SearchPageSeletors";
+
+export const SEARCH_PAGE = '[data-test"]';
